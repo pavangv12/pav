0cc29918e57cd85d0bb2ea9aa8a8de2d2eabde51
Pavan G V <pavan.gv@radiometer.dk>
2024-03-03 23:32:53 +0530

    font change and alignment issues

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,9 +9,9 @@ function Home() {
     <div className="home">
       <div className="about">
         <h2> Hi, I'm Pavan</h2>
-        <div className="prompt">
-          <p> I am a dedicated professional with a strong academic background in Computer Science and Engineering, holding a Bachelor's degree from PES College of Engineering and an M.Tech from M.S Ramaiah Institute of Technology. In my current role as a Software Engineer Intern at Beckman Coulter India Pvt. Ltd, Radiometer Opco, I actively contribute to Windows application development and log data analysis. I also have experience as a Database Administrator at Cisco through Tata Consultancy Services Pvt Ltd, where I managed databases and led disaster recovery initiatives.</p>
-          <p>I am passionate about research and innovation, as evidenced by projects such as analyzing Named Data Networks under Distributed Denial of Service attacks during my M.Tech final year. I have a diverse skill set including programming, databases, networking, and operating systems, and I'm keen to apply my expertise to tackle challenges in the dynamic technology landscape. With a strong commitment to learning, I am excited to bring my skills to an organization that values innovation and continuous growth. </p>
+              <div className="prompt">
+                  <h6 className="bio"> I am a dedicated professional with a strong academic background in Computer Science and Engineering, holding a Bachelor's degree from PES College of Engineering and an M.Tech from M.S Ramaiah Institute of Technology. In my current role as a Software Engineer Intern at Beckman Coulter India Pvt. Ltd, Radiometer Opco, I actively contribute to Windows application development and log data analysis. I also have experience as a Database Administrator at Cisco through Tata Consultancy Services Pvt Ltd, where I managed databases and led disaster recovery initiatives.</h6>
+                  <h6 className="bio">I am passionate about research and innovation, as evidenced by projects such as analyzing Named Data Networks under Distributed Denial of Service attacks during my M.Tech final year. I have a diverse skill set including programming, databases, networking, and operating systems, and I'm keen to apply my expertise to tackle challenges in the dynamic technology landscape. With a strong commitment to learning, I am excited to bring my skills to an organization that values innovation and continuous growth. </h6>
 
           <a
             href="https://www.linkedin.com/in/pavangv12/"
