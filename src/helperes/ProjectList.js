416448864ce159e973ab97fce58ef2aff1cb835e
Pavan G V <pavan.gv@radiometer.dk>
2025-02-13 01:18:38 +0530

    Updated project section

--- src/helperes/ProjectList.js
+++ src/helperes/ProjectList.js
@@ -2,25 +2,32 @@ import Proj1 from "../assets/ToDoList.png";
 import Proj2 from "../assets/Portfolio.jpg"
 import Proj3 from "../assets/Proj3.jpg";
 import Proj4 from "../assets/Proj4.jpeg"
-import Proj5 from "../assets/AppBreweryWallpaper 2.png";
-import Proj6 from "../assets/AppBreweryWallpaper 4.png"
-import Proj7 from "../assets/AppBreweryWallpaper 5.png"
+import Proj5 from "../assets/Kidneydisease_Image.png";
+import Proj6 from "../assets/airQuality.png";
+import Proj7 from "../assets/heartdisease.png";
+
 
 
 export const ProjectList = [
   {
-    name: "To-Do List App",
-    image: Proj1,
-    skills: "C#, WPF",
-        github: "https://github.com/pavangv12/ToDo-List-App",
+    name: "Deep learning Kidney Disease Classification ",
+    image: Proj5,
+    skills: "Python, tensorflow, DVC",
+    github: "https://github.com/pavangv12/Deep-learning-Kidney-Disease-Classification",
   },
   {
-    name: "Personal Portfolio",
-    image: Proj2,
-    skills: "React,Node.js,MongoDB",
-      github: "https://github.com/pavangv12/pav",
+    name: "India’s Air – Analysis and prediction of Air Quality in Indian cities",
+    image: Proj6,
+    skills: "Python, tensorflow",
+    github: "",
   },
   {
+    name: "Heart Disease Prediction",
+    image: Proj7,
+    skills: "Python, tensorflow",
+    github: "https://github.com/pavangv12/Heart-Disease-Prediction",
+  },
+    {
     name: "Stock Market Analysis and Prediction on Time Series Data",
     image: Proj3,
     skills: "Python, Machine learning",
@@ -33,21 +40,15 @@ export const ProjectList = [
     github: "https://github.com/pavangv12/NIFTY-50-Stock-Analysis",
   },
   {
-    name: "Social Media Website",
-    image: Proj5,
-    skills: "React,Node.js,MySQL,GraphQL",
-    github: "www.google.com",
+    name: "Personal Portfolio",
+    image: Proj2,
+    skills: "React,Node.js,MongoDB",
+    github: "https://github.com/pavangv12/pav",
   },
   {
-    name: "Dashboard Visualizer",
-    image: Proj6,
-    skills: "JavaScript,HTML,CSS",
-    github: "www.google.com",
-  },
-  {
-    name: "Mobile Game",
-    image: Proj7,
-    skills: "React Native,JavaScript,HTML,CSS",
-    github: "www.google.com",
+    name: "To-Do List App",
+    image: Proj1,
+    skills: "C#, WPF",
+        github: "https://github.com/pavangv12/ToDo-List-App",
   },
 ];
