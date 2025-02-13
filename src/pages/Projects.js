e2cd68b218fc1218ee4d1096caf5add9356f2357
Pavan G V <pavan.gv@radiometer.dk>
2025-02-13 23:42:59 +0530

    added publications

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -12,6 +12,39 @@ function Projects() {
                     <ProjectItem key={idx} id={idx} name={project.name} image={project.image} />
                 ))}
             </div>
+            <div className="publication-table-container">
+                <h2 className="publication-title">Publications</h2>
+                <table className="publication-table">
+                    <thead>
+                        <tr>
+                            <th>Sl. No</th>
+                            <th>Paper Name</th>
+                            <th>Publisher</th>
+                            <th>Link</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Non-Collusive Interest Flooding Attacks (NCIFA) in Named Data Networks using Rocketfuel ISP topology</td>
+                            <td>IEEE</td>
+                            <td><a href="https://ieeexplore.ieee.org/abstract/document/10469359" target="_blank" rel="noopener noreferrer">View</a></td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Survey on Security Risks in 5G Private Industrial Networks</td>
+                            <td>IEEE</td>
+                            <td><a href="https://ieeexplore.ieee.org/abstract/document/10057943" target="_blank" rel="noopener noreferrer">View</a></td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Web Application of Attendance and Result System</td>
+                            <td>IJISRT</td>
+                            <td><a href="https://www.ijisrt.com/assets/upload/files/IJISRT20APR188.pdf" target="_blank" rel="noopener noreferrer">View</a></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
